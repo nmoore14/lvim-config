101117273be1522b549a59df18cf82b2b6c07212
Nick Moore <nmoore@foreup.com>
2023-02-03 22:25:42 -0500

    Working on adding Swift to my config

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,5 +1,13 @@
 -- My Extra Plugins
 lvim.plugins = {
-    "rmehri01/onenord.nvim",
-    "vim-test/vim-test",
+    { "rmehri01/onenord.nvim" },
+    { "vim-test/vim-test" },
+    { "xbase-lab/xbase",
+        run = "make install",
+        requires = {
+            "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope.nvim",
+            "neovim/nvim-lspconfig"
+        },
+    }
 }
